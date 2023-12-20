b0b228dcdd3efb17863cb93817282a7bda48509a
Issah45 <90643234+Issah45@users.noreply.github.com>
2023-12-20 14:30:03 +0800

    Version 0.2!

--- main.py
+++ main.py
@@ -4,30 +4,21 @@ f = sys.argv[1]
 code = open(f, "r").read()
 
 standard = {
-	1: " ", 2: "a",
-	3: "b", 4: "c",
-	5: "d", 6: "e",
-	7: "f", 8: "g",
-	9: "i", 10: "j",
-	11: "k", 12: "l",
-	13: "m", 14: "n",
-	15: "o", 16: "p",
-	17: "q", 18: "r",
-	19: "s", 20: "t",
-	21: "u", 22: "v",
-	23: "w", 24: "x",
-	25: "y", 26: "z",
-	27: ".", 28: ",",
-	29: "!", 30: "'",
-	31: "\"", 32: "?",
-	33: "#", 34: "h.inev",
-	35: "+", 36: "-",
-	37: "h", 38: "\n",
-	39: "0", 40: "1",
-	41: "2", 42: "3",
-	43: "4", 44: "5",
-	45: "6", 46: "7",
-	47: "8", 48: "9"
+	1: "a", 2: "b", 3: "c",
+	4: "d", 5: "e", 6: "f",
+	7: "g", 8: "h", 9: "i",
+	10: "j", 11: "k", 12: "l",
+	13: "m", 14: "n", 15: "o",
+	16: "p", 17: "q", 18: "r",
+	19: "s", 20: "t", 21: "u",
+	22: "v", 23: "w", 24: "x",
+	25: "y", 26: "z", 27: "1",
+	28: "2", 29: "3", 30: "4",
+	31: "5", 32: "6", 33: "7",
+	34: "8", 35: "9", 36: "0",
+	37: " ", 38: ".", 39: ",",
+	40: "!", 41: "#", 42: "$",
+	43: "'", 44: "\"", 45: "|",
 }
 
 def interprete(what):
@@ -52,16 +43,14 @@ def interprete(what):
 			position += 1
 		if letter == "<":
 			position -= 1
-		if letter == "p":
+		if letter == "*":
 			print(position)
-		if letter == "q":
-			print(array[position])
 		if letter == "o":
 			for number in array:
 				print(number, end="")
 		
 		# Displaying Things & Input
-		if letter == ".":
+		if letter == "&":
 			print(standard[array[position]], end="")
 		if letter == "i":
 			array[position] = int(input())
@@ -71,13 +60,13 @@ def interprete(what):
 			else:
 				array[position] = 0
 			array[position+1] = 0
-		if letter == "e":
+		if letter == "/":
 			array[position+1] = array[position]
 			array[position] = 0
 			position += 1
 		
 		# Math
-		if letter == "v":
+		if letter == "\\":
 			array.pop(position)
 			position -= 1
 			array.append(0)
@@ -93,8 +82,11 @@ def interprete(what):
 		if letter == "z":
 			array[position] = round(array[position] / array[position + 1])
 			array[position + 1] = 0
+		
+		if letter == "%":
+			print("Arraylang Version 0.2")
 
 try:
 	interprete(code)
 except:
-	print("there is something wrong with your (bad) code")
+	print("there is something wrong with your (terrible) code")
