0813f5e5d0751c187e605d03d4b7ae5868e77912
Issah45 <90643234+Issah45@users.noreply.github.com>
2024-12-13 05:49:47 -0800

    i's an lts

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# New Arraylang
-In New Arraylang, data is stored on a 16-bit 1 dimensional-stack (although it can be infinite with the `A` function). You can move through the stack and change values. Arraylang also has modes, which are used for loops, if statements, and variables\
+# Arraylang
+In Arraylang, data is stored on a 16-bit 1 dimensional-stack (although it can be infinite with the `d` function). You can move through the stack and change values. Arraylang also has modes, which are used for loops and if statements\
 
 ## Commands
 `>` Moves to the next cell\
@@ -11,6 +11,7 @@ In New Arraylang, data is stored on a 16-bit 1 dimensional-stack (although it ca
 `c` Increases the value of the current cell by 5\
 `*` Displays the current cell number\
 `o` Displays the entire array\
+`&` Displays the value of the current cell as a string using Issanium Code\
 `i` Gets an integer input\
 `/` Moves the value of the current cell to the next cell, emptying the current cell, and moving the pointer to the next cell\
 `\` Empties the current cell\
@@ -19,13 +20,26 @@ In New Arraylang, data is stored on a 16-bit 1 dimensional-stack (although it ca
 `y` Multiplies the current cell by the next cell, emptying the next cell\
 `z` Divides the current cell by the next cell, emptying the next cell\
 `q` Prints the current cell value\
+`d` Adds a new cell to the array\
 `$` If the cell after the current cell is equal to the current cell's value, the current cell will be 1, if not the current cell will be zero, then the cell after the current cell will be set back to zero\
-`A` adds an empty cell to the array\
-`()` runs the code inside the brackets for a certain number of times e.g. `(++q)5`\
-`V` variable setter (`Va5` means a = 5. If it is `Vai`, it will take an input and if it is `Va?`, then it will use the current cell as a variable)\
-`B` variable getter (`Ba` sets the current cell to the variable a)\
-`R` removes a variable (`Ra` removes the a variable)
-`Q` prints out all variables and their values\
-`N` prints out a new line\
-`"` prints out a string e.g. `"Hello"` would print out Hello\
-`{}` uses Tkinter to show an alert e.g. `{hello}` would make an alert saying hello\
+`()` runs the code inside the brackets for a certain number of times e.g. `(++q)5`
+
+## Issanium Code
+`
+1: "a", 2: "b", 3: "c",
+4: "d", 5: "e", 6: "f",
+7: "g", 8: "h", 9: "i",
+10: "j", 11: "k", 12: "l",
+13: "m", 14: "n", 15: "o",
+16: "p", 17: "q", 18: "r",
+19: "s", 20: "t", 21: "u",
+22: "v", 23: "w", 24: "x",
+25: "y", 26: "z", 27: "1",
+28: "2", 29: "3", 30: "4",
+31: "5", 32: "6", 33: "7",
+34: "8", 35: "9", 36: "0",
+37: " ", 38: ".", 39: ",",
+40: "!", 41: "#", 42: "$",
+43: "'", 44: "\"", 45: "|",
+46: "\n"
+`
--- arraylang.py
+++ arraylang.py
@@ -1,47 +1,82 @@
-
-import sys, os, tkinter
+import sys
 
 f = sys.argv[1]
 code = open(f, "r").read()
 
-array_normal = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-variables = {}
+standard = {
+    1: "a", 2: "b", 3: "c",
+    4: "d", 5: "e", 6: "f",
+    7: "g", 8: "h", 9: "i",
+    10: "j", 11: "k", 12: "l",
+    13: "m", 14: "n", 15: "o",
+    16: "p", 17: "q", 18: "r",
+    19: "s", 20: "t", 21: "u",
+    22: "v", 23: "w", 24: "x",
+    25: "y", 26: "z", 27: "1",
+    28: "2", 29: "3", 30: "4",
+    31: "5", 32: "6", 33: "7",
+    34: "8", 35: "9", 36: "0",
+    37: " ", 38: ".", 39: ",",
+    40: "!", 41: "#", 42: "$",
+    43: "'", 44: "\"", 45: "|",
+    46: "\n"
+}
 
-def interprete(what, array=array_normal, position=0):
+
+def interprete(what,
+               array=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+               position=0):
     mode = "code"
     loop = ""
-    
+
     for letter in what:
+        # Array Position
         if mode == "code":
-            # Arithmetic
             if letter == "+":
                 array[position] += 1
             if letter == "-":
                 array[position] -= 1
-            
-            # Pointer
             if letter == "a":
                 array[position] += 20
             if letter == "b":
                 array[position] += 10
             if letter == "c":
                 array[position] += 5
-            
+
+            # Moving The Pointer
             if letter == ">":
                 position += 1
             if letter == "<":
                 position -= 1
-            
+            if letter == "*":
+                print(position)
+            if letter == "o":
+                for number in array:
+                    print(number, end="")
+
+            # Displaying Things & Input
+            if letter == "&":
+                print(standard[array[position]], end="")
+            if letter == "i":
+                array[position] = int(input())
+            if letter == "0":
+                if array[position + 1] == 0:
+                    array[position] = 1
+                else:
+                    array[position] = 0
+                array[position + 1] = 0
             if letter == "/":
-                array[position+1] = array[position]
+                array[position + 1] = array[position]
                 array[position] = 0
                 position += 1
-            
-            if letter == "\\":
-                array[position-1] = array[position]
-                array[position] = 0
-                position -= 1
+            if letter == "q":
+                print(array[position])
 
+            # Math
+            if letter == "\\":
+                array.pop(position)
+                position -= 1
+                array.append(0)
             if letter == "w":
                 array[position] = array[position] + array[position + 1]
                 array[position + 1] = 0
@@ -54,61 +89,19 @@ def interprete(what, array=array_normal, position=0):
             if letter == "z":
                 array[position] = round(array[position] / array[position + 1])
                 array[position + 1] = 0
-            
-            # Array
             if letter == "A":
                 array.append(0)
-            
-            # Input
-            if letter == "i":
-                array[position] = int(input())
-            
-            # Output
-            if letter == "o":
-                for number in array:
-                    print(number, end="|")
-            
-            if letter == "*":
-                print(position)
-            if letter == "q":
-                print(array[position])
-            
-            if letter == "N":
-                print()
-            
-            if letter == "Q":
-                for v in variables:
-                    print(f"{v} = {variables[v]}")
-            
-            # Modal Commands
+
+            if letter == "%":
+                print("Arraylang Version 0.2")
+
             if letter == "$":
                 mode = "if"
-            
+
             if letter == "(":
                 loop = ""
                 mode = "loop"
 
-            if letter == "V":
-                mode = "var_set"
-            if letter == "B":
-                mode = "var_get"
-            if letter == "R":
-                mode = "var_delete"
-            
-            if letter == '"':
-                string = ""
-                mode = "string"
-            
-            # Others
-            if letter == "0":
-                array[position] = 0
-            
-            # Graphics
-            if letter == "{":
-                mode = "alert"
-                q = ""
-        
-        # Modes
         elif mode == "if":
             if array[position + 1] == int(letter):
                 array[position + 1] = 0
@@ -117,54 +110,15 @@ def interprete(what, array=array_normal, position=0):
                 array[position + 1] = 0
                 array[position] = 0
             mode = "code"
-        
-        elif mode == "string":
-            if letter == "\"":
-                print(string, end="")
-                mode = "code"
-            string += letter
-        
+
         elif mode == "loop":
             if letter == ")":
                 mode = "loop2"
             loop += letter
-        
+
         elif mode == "loop2":
             for i in range(int(letter)):
                 interprete(loop, array, position)
-
-        elif mode == "var_set":
-            variables[letter] = "N"
-            mode = "var_set_2"
-            l = letter
-
-        elif mode == "var_set_2":
-            if letter == "i":
-                variables[l] = int(input())
-            elif letter == "?":
-                variables[l] = array[position]
-            else:
-                variables[l] = int(letter)
-            mode = "code"
-
-        elif mode == "var_get":
-            n = variables[letter]
-            array[position] = n
-            mode = "code"
-
-        elif mode == "var_delete":
-            variables.pop(letter)
-            mode = "code"
-        
-        elif mode == "alert":
-            if letter == "}":
-                r = tkinter.Tk()
-                txt = tkinter.Label(r, text=q)
-                txt.pack()
-                r.mainloop()
-                mode = "code"
-            else:
-                q += letter
 
 
 interprete(code)
